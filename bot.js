61538351423d1044276f43c5c266de32f8665354
Fabio <30632870+JustFabiolous@users.noreply.github.com>
2017-09-20 17:05:01 +0200

    changed member join channel

--- bot.js
+++ bot.js
@@ -87,7 +87,7 @@ client.on('message', message => {
 //message on member join
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server
-  const channel = member.guild.channels.find('name', 'ext-logs');
+  const channel = member.guild.channels.find('name', 'announcements');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
