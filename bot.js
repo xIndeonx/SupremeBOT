0666200076fd385f27dd82292a823c004980633c
Fabio <30632870+JustFabiolous@users.noreply.github.com>
2017-09-20 23:32:56 +0200

    added a comment

--- bot.js
+++ bot.js
@@ -90,6 +90,7 @@ client.on('message', function(message) {
   }
 });
 
+//music stuff
 client.on('message', async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(PREFIX)) return;
