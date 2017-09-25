e02a5cad6a5ad2e0bb26d85fec678a7a9cf56366
Lucas <lucaskvn@gmail.com>
2017-09-25 14:49:48 +0200

    Added purge function

--- bot.js
+++ bot.js
@@ -207,6 +207,26 @@ client.on('message', function(message) {
     } else {
       message.channel.send('You are not authorized to use this command.');
     }
+  } else if (message.content === `${PREFIX}purge`) { //purge Messages
+      if ((message.author.id === OWNERID) || (message.author.id === LUCASID)) {
+        if (message.channel.type == 'text') {
+            message.channel.fetchMessages()
+              .then(messages => {
+                message.channel.bulkDelete(messages);
+                messagesDeleted = messages.array().length; // number of messages deleted
+    
+                // Logging the number of messages deleted on both the channel and console.
+                message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
+                console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+              })
+              .catch(err => {
+                console.log('Error while doing Bulk Delete');
+                console.log(err);
+              });
+          }
+      } else {
+        message.channel.send('You are not authorized to use this command.');
+    }
   } else if (message.content.startsWith(`${PREFIX}setAvatar`)) { //setAvatar
       if ((message.author.id === OWNERID) || (message.author.id === LUCASID)) {
         var input = message.content;
