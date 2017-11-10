618cc673a409a458eb2a6173db041bea9836a57d
Lucas <lucaskvn@outlook.com>
2017-11-10 07:57:35 +0100

    Added ban command

--- modules/commands.js
+++ modules/commands.js
@@ -278,6 +278,57 @@ commands = function () {
 			})
 				.catch(err => logToChannel('Error', err, message.author.tag, message.author.displayAvatarURL));
 		}
+		else if (message.content.toLowerCase().startsWith(`${constants.PREFIX}ban`)) { // ban
+			if ((message.member.permissions.has('ADMINISTRATOR')) || (message.author.id === constants.OWNERID) || (message.author.id === constants.OWNERID)) {
+				const member = message.mentions.members.first();
+				if (!member) {
+					return message.channel.send({
+						embed: {
+							title: 'Error',
+							color: constants.red,
+							description: 'Please mention a valid member of this server.'
+						}
+					});
+				}
+				if (!member.bannable) {
+					return message.channel.send({
+						embed: {
+							title: 'Error',
+							color: constants.red,
+							description: 'I cannot ban this user! Do they have a higher role? Do I have the `Ban Members` permission?'
+						}
+					});
+				}
+				const reason = args.slice(2).join(' ');
+				if (!reason) {
+					return message.channel.send({
+						embed: {
+							title: 'Error',
+							color: constants.red,
+							description: 'Please indicate a reason for the ban!'
+						}
+					});
+				}
+				member.ban(reason)
+					.catch(error => message.channel.send({
+						embed: {
+							title: 'Error',
+							color: constants.red,
+							description: `Sorry ${message.author}, I couldn't ban the user.\n **Error**: ${error}`
+						}
+					}));
+				message.react('✅');
+			}
+			else {
+				return message.channel.send({
+					embed: {
+						title: 'Error',
+						color: constants.red,
+						description: 'Sorry, you don\'t have permissions to use this!'
+					}
+				});
+			}
+		}
 		else if (message.content.toLowerCase().startsWith(`${constants.PREFIX}channelinfo`)) { // channelinfo
 			try {
 				const embed = new constants.Discord.RichEmbed()
