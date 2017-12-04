cc42672b52eefd4ddaa75797114041ad726f181f
Fabio <30632870+JustFabiolous@users.noreply.github.com>
2017-12-04 19:32:56 +0100

    reversed change

--- modules/commands.js
+++ modules/commands.js
@@ -440,7 +440,7 @@ commands = function () {
 				else {
 					voiceChannel.join()
 						.then(connection => {
-							const dispatcher = connection.playFile('./files/feather.mp3');
+							const dispatcher = connection.playFile('./files/airhorn.mp3');
 							dispatcher.on('end', () => {
 								voiceChannel.leave();
 							});
