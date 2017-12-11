b07afc147e82c97e1b7a127e4c8156961b45d481
Fabio <30632870+JustFabiolous@users.noreply.github.com>
2017-12-11 16:41:09 +0100

    update

--- modules/constants.js
+++ modules/constants.js
@@ -29,8 +29,8 @@ const queue = new Map();
 const embed = new Discord.MessageEmbed();
 const type = 3;
 const GAME = `you | ${PREFIX}help`;
-const update = 'December 10th, 2017';
-const version = 'Alpha 0.3.0.7';
+const update = 'December 11th, 2017';
+const version = 'Alpha 0.3.0.8';
 
 // airbrake
 const airbrakeJs = require('airbrake-js');
